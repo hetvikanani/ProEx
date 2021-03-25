2fe0cdc4d56d83828e11bf8163c1771ed23b3245
hetvipatel3110@gmail.com <hetvipatel3110@gmail.com>
2021-03-25 18:49:21 +0530

    datatable

--- script2.js
+++ script2.js
@@ -1,56 +1,21 @@
-let data = [
-  {
-    city: "Rajkot",
-    email: "ffd",
-    firstName: "hetvi",
-    gender: "female",
-    lastName: "kanani",
-    mobile: "65566",
-  },
-  // {
-  //   email: "ffd",
-  //   firstName: "chaku",
-  //   gender: "female",
-  //   lastName: "kanani",
-  // },
-  // {
-  //   email: "ffd",
-  //   firstName: "vasu",
-  //   gender: "female",
-  //   lastName: "kanani",
-  // },
-];
-const constLimit = 5;
-const constPageNumber = 1;
-const customPagination = document.getElementById("customPagination");
-
-console.log(data);
 var modal = document.getElementById("myModal");
-var myModalTwo = document.getElementById("myModalTwo");
 var btn = document.getElementById("myBtn");
 var span = document.getElementsByClassName("close")[0];
-var spantwo = document.getElementsByClassName("closetwo")[0];
-console.log(spantwo, "done");
 btn.onclick = function () {
   modal.style.display = "block";
 };
 span.onclick = function () {
   modal.style.display = "none";
 };
-
 window.onclick = function (event) {
   if (event.target == modal) {
     modal.style.display = "none";
   }
-  if (event.target == myModalTwo) {
-    myModalTwo.style.display = "none";
-  }
 };
 // modal finish
-
+let data = [];
 document.getElementById("updatebtn").addEventListener("click", updatadata);
 var form = document.getElementById("form");
-
 // calling
 const fname = document.getElementById("fname");
 const lname = document.getElementById("lname");
@@ -60,23 +25,18 @@ const birthday = document.getElementById("birthday");
 const city = document.getElementById("city");
 const state = document.getElementById("state");
 const country = document.getElementById("country");
-
 let spaname = document.getElementById("spaname");
 var spalname = document.getElementById("spalname");
 var spaeiname = document.getElementById("spaeiname");
 var spaminame = document.getElementById("spaminame");
-var btnsearch = document.getElementById("btnsearch");
-
 function validateEmail(ename) {
   const re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
   return re.test(ename);
-}
-
+};
 if (form) {
   form.addEventListener("submit", add);
 }
-
-function add(event) {
+function add(e) {
   event.preventDefault();
   if (fname.value.trim() === "") {
     spaname.innerHTML = "plz fill the field";
@@ -90,16 +50,17 @@ function add(event) {
     spaeiname.innerHTML = "please fill the field";
     return false;
   }
-//   if (ename.value != validateEmail(ename)) {
-//     spaeiname.innerHTML = "plz proper email";
-// }
+  // if (ename.value != validateEmail(ename)) {
+  //   spaeiname.innerHTML = "plz proper email";
+  // }
   if (mname.value.trim() === "") {
-    spaminame.innerHTML = "please fill the field";
+    spaminame.innerHTML = "please fill the field"
     return false;
   }
 
   const gname = document.querySelector('input[name="optradio"]:checked');
-  const detail = {
+
+const detail = {
     id: fname.value,
     firstName: fname.value,
     lastName: lname.value,
@@ -112,27 +73,18 @@ function add(event) {
     country: country.value,
   };
   data.push(detail);
-  console.log(data);
   loadData();
   modal.style.display = "none";
-
   fname.value = "";
   lname.value = "";
   ename.value = "";
   mname.value = "";
-  // gname.value = "";
+  gname.value = "";
   birthday.value = "";
   city.value = "";
   state.value = "";
   country.value = "";
 }
-
-function searchh() {
-  var search = document.getElementById("search").value;
-  if (search) loadData(data.filter((d) => d.firstName.includes(search)));
-  else loadData();
-}
-
 function remove(event) {
   data = data.filter((element) => {
     return element.id !== event.target.id;
@@ -140,26 +92,19 @@ function remove(event) {
   loadData();
 }
 var modaltwo = document.getElementById("myModalTwo");
-
+var btnjevu = document.getElementById("btnjevu");
 var spantwo = document.getElementsByClassName("closetwo")[0];
 spantwo.onclick = function () {
   modaltwo.style.display = "none";
 };
-
 let updateid = null;
+
 function update(event) {
   updateid = event.target.id;
   modaltwo.style.display = "block";
-  document
-    .getElementById("closeButtonForUpdateModal")
-    .addEventListener("click", () => {
-      console.log("hi");
-      modaltwo.style.display = "none";
-    });
   let ans = data.filter((gme) => {
     return gme.id === event.target.id;
   });
-
   const fname = document.getElementById("fnametwo");
   const lname = document.getElementById("lnametwo");
   const ename = document.getElementById("enametwo");
@@ -168,87 +113,37 @@ function update(event) {
   const city = document.getElementById("citytwo");
   const state = document.getElementById("statetwo");
   const country = document.getElementById("countrytwo");
-  const genamefemale = document.getElementById("genamefemaleupdate");
-  const genamemale = document.getElementById("gnamemaleupdate");
-
+  const genamefemale = document.getElementById("genamefemale");
+  const genamemale = document.getElementById("genamemale");
   fname.value = ans[0].firstName;
   lname.value = ans[0].lastName;
   ename.value = ans[0].email;
   mname.value = ans[0].mobile;
-
   if (ans[0].gender === "male") {
     genamemale.checked = true;
   } else if (ans[0].gender === "female") genamefemale.checked = true;
-
   birthday.value = ans[0].birthday;
   city.value = ans[0].city;
   state.value = ans[0].state;
   country.value = ans[0].country;
 }
-
-const changePagination = (e) => {
-  loadData(null, +e.target.id.split("-")[1]);
-  console.log(document.getElementById(e.target.id).parentElement);
-  document.getElementById(e.target.id).classList.add("activePageBorder");
-};
-function loadData(newData, pageNumber) {
-  let finalPageNumber = constPageNumber;
-  if (pageNumber) finalPageNumber = pageNumber;
-  // const totalUserCount = document.getElementById("totalUserCount");
-  // console.log(newData);
-  let finalData = data;
-  if (newData) finalData = newData;
+function loadData() {
   const table = document.getElementById("table");
-  // totalUserCount.innerText = finalData.length;
   let rows = "";
-  // let paginationUi = "";
-  // const totalPage = Math.ceil(finalData.length / constLimit);
-  // for (let i = 0; i < totalPage; i++) {
-  //   if (i === 0 && finalPageNumber === 1)
-  //     paginationUi =
-  //       paginationUi +
-  //       `<li class="page-item "  onclick="changePagination(event)" ><a  id='page-${
-  //         i + 1
-  //       }'
-  //     data-id='page-${
-  //       i + 1
-  //     }'style="margin: 0.5rem;" class="page-link activePageBorder" href="#">${
-  //         i + 1
-  //       }</a></li>`;
-  //   else
-  //     paginationUi =
-  //       paginationUi +
-  //       `<li class="page-item"  onclick="changePagination(event)" ><a id='page-${
-  //         i + 1
-  //       }'
-  //     data-id='page-${
-  //       i + 1
-  //     }'style="margin: 0.5rem;" class="page-link" href="#">${i + 1}</a></li>`;
-  // }
-  // paginationUi =
-  //   paginationUi +
-  //   `<div>Total User : <span id='totalUserCount'>${finalData.length}</span></div>`;
-
-  // customPagination.innerHTML = paginationUi;
-  // let start = (finalPageNumber - 1) * constLimit;
-  // let end = finalPageNumber * constLimit;
-  // let finalEnd = end;
-  // if (finalEnd > finalData.length) finalEnd = finalData.length;
-
-  for (let i = start; i < finalEnd; i++) {
+  for (let i = 0; i < data.length; i++) {
     rows =
       rows +
       `<tr>
-
-             <td>${finalData[i].firstName}</td>
-             <td>${finalData[i].lastName}</td>
-             <td>${finalData[i].email}</td>
-             <td>${finalData[i].mobile}</td>
-             <td>${finalData[i].gender}</td>
-             <td>${finalData[i].birthday}</td>
-             <td>${finalData[i].city}</td>
-             <td>${finalData[i].state}</td>
-             <td>${finalData[i].country}</td>
+            
+             <td>${data[i].firstName}</td>
+             <td>${data[i].lastName}</td>
+             <td>${data[i].email}</td>
+             <td>${data[i].mobile}</td>
+             <td>${data[i].gender}</td>
+             <td>${data[i].birthday}</td>
+             <td>${data[i].city}</td>
+             <td>${data[i].state}</td>
+             <td>${data[i].country}</td>
              <th><i class="fa fa-pencil" aria-hidden="true" id="${data[i].id}" onclick="update(event)" style="padding-right: 10px;"></i>
              <i class="fa fa-trash-o" aria-hidden="true" id="${data[i].id}"  onclick="remove(event)"></i> </th>
  </tr>`;
@@ -256,17 +151,14 @@ function loadData(newData, pageNumber) {
   table.append(rows);
   table.innerHTML = rows;
 }
-
 function updatadata(event) {
   event.preventDefault();
-
   const fname = document.getElementById("fnametwo").value;
   const lname = document.getElementById("lnametwo").value;
   const ename = document.getElementById("enametwo").value;
   const mname = document.getElementById("mnametwo").value;
   const gname = document.querySelector('input[name="optradiotwo"]:checked')
     .value;
-
   const birthday = document.getElementById("birthdaytwo").value;
   const city = document.getElementById("citytwo").value;
   const state = document.getElementById("statetwo").value;
@@ -283,7 +175,7 @@ function updatadata(event) {
     state: state,
     country: country,
   };
-
+  console.log(updateid);
   for (let i = 0; i < data.length; i++) {
     if (data[i].id === updateid) {
       data[i] = detail;
@@ -298,27 +190,19 @@ fname.addEventListener("keypress", (e) => {
     spaname.innerHTML = "";
   }
 });
-
 lname.addEventListener("keypress", (e) => {
   if (e.target.value.trim() != "");
   {
     spalname.innerHTML = "";
   }
 });
-
 ename.addEventListener("keypress", (e) => {
-  if (e.target.value.trim() != "" && e.target.value === validateEmail(ename));
-  {
+  if (e.target.value.trim() != "" && e.target.value === validateEmail(ename)); {
     spaeiname.innerHTML = "";
   }
-  return false;
 });
-
-mname.addEventListener("keypress", (e) => {
-  if (e.target.value.trim() != "");
-  {
+mname.addEventListener('keypress', (e) => {
+  if (e.target.value.trim() != ""); {
     spaminame.innerHTML = "";
   }
 });
-
-loadData();
